366393b99af630ea4bed11137dd5bd08ed883e48
fabiodm7 <fabio.d.m@hotmail.com>
2021-06-28 23:16:17 -0300

    ordenação 1

--- modEx-estruturaDados/bolha.js
+++ modEx-estruturaDados/bolha.js
@@ -1,14 +1,16 @@
 var valores = [8,7,6,5,4,3,2,1];
 function ordena(){
     let inicio = 0;
-    let fim = 9;
+    let fim = 8;
     let tmp;
 
-    for (pos = inicio; pos < fim -1; pos++){
-        if (valores[pos] > valores[pos +1]){
-            tmp = valores[pos];
-            valores[pos] = valores[pos +1];
-            valores[pos+1] = tmp;
+    for (vezes = 0; vezes < 8; vezes++){
+        for (pos = inicio; pos < fim -1-vezes; pos++){
+            if (valores[pos] > valores[pos +1]){
+                tmp = valores[pos];
+                valores[pos] = valores[pos +1];
+                valores[pos+1] = tmp;
+            }
         }
     }
 }

ad5021c36c3ce5dee86b4dde44976d3e1ac6946e
fabiodm7 <fabio.d.m@hotmail.com>
2021-06-30 22:42:08 -0300

    footer

--- template-node-fulljs/src/views/pages/components/float/floatImage.js
+++ template-node-fulljs/src/views/pages/components/float/floatImage.js
@@ -2,7 +2,7 @@ import yodaBaby from '../../../../assets/images/elements/baby-yoda 1.png';
 
 let floatImage = `
 <div class="main-left">
-    <img src= "${yodaBaby}" alt="Baby Yoda">
+    <img src= "${yodaBaby}" alt="Baby Yoda" width="120px" heigth="auto">
 </div>
 `
 
--- template-node-fulljs/src/views/pages/components/sections/homeSectionOne.js
+++ template-node-fulljs/src/views/pages/components/sections/homeSectionOne.js
@@ -2,10 +2,12 @@ import logo from '../../../../assets/images/logo/logo.png';
 import backgroundOne from '../../../../assets/images/background/bg1.png';
 
 let HomeSectionOne = `
-<section class="section backgroud--one" style="backgroud-image: url(${backgroundOne})>
-    <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
-    <img src=${logo} alt="Logo Star Wars" width="300px" heigth="auto">
-</section>
+    <section class="section backgroud--one" style="backgroud-image: url(${backgroundOne})>
+        <div class="block-section--one">
+            <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
+            <img src=${logo} alt="Logo Star Wars" width="300px" heigth="auto">
+        </div>
+    </section>
 `
 
 export default HomeSectionOne

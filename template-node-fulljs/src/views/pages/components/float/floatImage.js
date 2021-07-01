4bd0d9a55f035136d19a89dd2fd8df358f935007
fabiodm7 <fabio.d.m@hotmail.com>
2021-06-30 23:58:08 -0300

    nem ideia do que ele fez exatamente

--- template-node-fulljs/src/views/pages/components/float/floatImage.js
+++ template-node-fulljs/src/views/pages/components/float/floatImage.js
@@ -2,6 +2,7 @@ import yodaBaby from '../../../../assets/images/elements/baby-yoda 1.png';
 
 let floatImage = `
 <div class="main-left">
+<a href="#section-two"
     <img src= "${yodaBaby}" alt="Baby Yoda" width="120px" heigth="auto">
 </div>
 `
--- template-node-fulljs/src/views/pages/components/footer/index.js
+++ template-node-fulljs/src/views/pages/components/footer/index.js
@@ -1,9 +1,9 @@
 import deathStar from '../../../../assets/images/elements/planet.png';
 
 let footer = `
-    <div>
+    <div class="footer-contain">    
         <p>
-            Star wars  -  este é um exemplo criado para os Alunos Gama Academy
+            Star wars  -  este é um exemplo criado para os Alunos Gama Academy</br>
             Módulo de boas práticas.
         </p>
         <img src=${deathStar} alt="Death Star" heigth="180px" width="auto">

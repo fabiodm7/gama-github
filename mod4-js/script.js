dd163953d0c8c38ff0fc5d23219900375c5e5b38
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-05 07:31:32 -0300

    operadores

--- mod4-js/script.js
+++ mod4-js/script.js
@@ -27,32 +27,82 @@
 
 // console.log(myLastName)
 
-// Javascript é dinamicamente tipado
-var myName = 'Fabio' //string
-var age = 32 //number
-var height = 1.58 //number
+// // Javascript é dinamicamente tipado
+// var myName = 'Fabio' //string
+// var age = 32 //number
+// var height = 1.58 //number
 
-null //objeto vazio
-undefined //objetvo não definido
+// null //objeto vazio
+// undefined //objetvo não definido
 
-var isStudent = false //boolean
+// var isStudent = false //boolean
 
-// objeto, array e function
-var person = {
-    "name": "Fabio",
-    "age": 32
-}
+// // objeto, array e function
+// var person = {
+//     "name": "Fabio",
+//     "age": 32
+// }
 
-var students = ["Fabio", "Maria", "Joao"]
+// var students = ["Fabio", "Maria", "Joao"]
 
-function soma(a,b){
-    return a+b
-}
+// function soma(a,b){
+//     return a+b
+// }
 
-// operador typeof
+// // operador typeof
 
-console.log(typeof 32); //number
-console.log(typeof age); //number
-console.log(typeof isStudent);//boolean
-console.log(typeof null);//object
-console.log(typeof soma);//function
+// console.log(typeof 32); //number
+// console.log(typeof age); //number
+// console.log(typeof isStudent);//boolean
+// console.log(typeof null);//object
+// console.log(typeof soma);//function
+
+// // operadores atirméticos
+// // adição +
+// var sumTwoNumber = 1 + 3
+// console.log(sumTwoNumber)
+// // subtração -
+// var subTwoNumber = 1 - 3
+// console.log(subTwoNumber)
+// // multiplicação *
+// var multTwoNumber = 1 * 3
+// console.log(multTwoNumber)
+// // divisão /
+// var divTwoNumber = 1 / 3
+// console.log(divTwoNumber)
+// // módulo, resto da divisão %
+// var modTwoNumber = 1 % 3
+// console.log(modTwoNumber)
+// // incremento ++
+// var incremento = 1
+// console.log(++incremento)
+// // decremento --
+// var decremento = 2
+// console.log(--decremento)
+
+// // operadores de atribuição
+// // = atribuição
+// // += atribuição de soma
+// var x = 1, y = 3
+// console.log(x += y)
+// // -= atribuição de subtração
+// console.log(x -= y)
+// // *= atribuição de multiplicação
+// console.log(x *= y)
+// // /= atribuição de divisão
+// console.log(x /= y)
+// // %= atribuição de módulo
+// console.log(x %= y)
+
+// // operadores de comparação
+// // == comparação de valores
+// // === coparação de valores e tipos
+// // != diferença entre valores
+// // < ou <= menor ou menorIgual
+// // > ou >= maior ou maiorIgual
+
+// // operadores lógicos
+// // e &&
+// // ou ||
+// // negação !
+

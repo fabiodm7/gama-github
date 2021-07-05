f856033bbcda4484675d4e5ba681974d0e5be736
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-05 08:12:19 -0300

    classe

--- mod4-js/script.js
+++ mod4-js/script.js
@@ -167,22 +167,70 @@
 //     i++;
 // } while ( i < 10 );
 
-// funções
-function sayHello(){
-    console.log("Hello");
+// // funções
+// function sayHello(){
+//     console.log("Hello");
+// }
+
+// sayHello();
+
+// function sayHello2(name, lastName){
+//     console.log("Hello " + name + " " + lastName);
+// }
+
+// sayHello2();
+// sayHello2("Fabio", "Medina");
+
+// function sum(a,b){
+//     return a + b;
+// }
+
+// console.log(sum(40,60));
+
+// classes
+class Book {
+    constructor(title, author,pages){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    read(){
+        return `Estou lendo ${this.title} de ${this.author}`
+    }
 }
 
-sayHello();
+let book = new Book('Algoritmos para viver', 'Brian', 500);
+console.log(book);
+console.log(book.read());
 
-function sayHello2(name, lastName){
-    console.log("Hello " + name + " " + lastName);
+let otherBook = new Book('Um fulano','beltrano',100);
+console.log(book,otherBook);
+
+class ITBook extends Book{
+    constructor(title, author, pages,technology){
+        super(title, author, pages);
+        this.technology = technology;
+    }
 }
 
-sayHello2();
-sayHello2("Fabio", "Medina");
+let itBook = new ITBook('Algoritmos para viver', 'Brian', 500,'Algoritmos')
+console.log(itBook.read());
 
-function sum(a,b){
-    return a + b;
+class Person {
+    constructor(name){
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(value){
+        this._name = value;
+    }
 }
 
-console.log(sum(40,60));
+let person = new Person('Fabio');
+person.name = 'Medina';
+console.log(person.name);

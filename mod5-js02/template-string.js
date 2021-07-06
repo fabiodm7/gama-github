0ba46846fd34dc8d3677282db54ea8628043cf27
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-05 23:14:17 -0300

    arrays, spread, metodos e objetos destructuring

--- mod5-js02/template-string.js
+++ mod5-js02/template-string.js
@@ -39,3 +39,92 @@
 // const sayHello = name => `Hello ${name}`
 
 // console.log(sayHello('Fabio'))
+
+// // Arrays, Spread & métodos
+// const alunasGama = ["Paula","Maria","Estela","Clara"]
+
+// // acessar informações num array
+// console.log(alunasGama[2]) //Estela
+
+// // operador spread (...)
+// const alunasXp = [...alunasGama,"Simara"]
+
+// console.log(alunasXp)
+
+// // Métodos de iteração
+// for (let i = 0; i < alunasXp.length; i++) {
+//     console.log(alunasXp[i])
+// }
+
+// // Map
+// alunasXp.map(aluna => console.log(aluna))
+
+// // Filter
+// const numeros = [34,45,67,90,55,76]
+
+// const numerosImpares = numeros.filter(numero => numero % 2 != 0)
+// console.log(numerosImpares)
+
+// const numerosPares = numeros.filter(numero => numero % 2 == 0)
+// console.log(numerosPares)
+
+// // Sort
+// const numerosOrdenadosCrescente = numeros.sort()
+// console.log(numerosOrdenadosCrescente)
+
+// const numerosOrdenadosDecrescente = numeros.sort((a,b) => b-a)
+// console.log(numerosOrdenadosDecrescente)
+
+// // Reduce
+// // Reduz a array a um resultado de uma operação matemática
+// const numbers = [1,34,35]
+
+// const soma = numbers.reduce((valorAnterior,valorAtual) => {
+//     return valorAnterior + valorAtual
+// },0) //75
+
+// console.log(soma)
+
+// Objetos Destructuring
+const pessoa = {
+    nome: "Fabio",
+    idade: 31,
+    cidade: "Sao Paulo"
+}
+
+// notação de ponto
+console.log(pessoa.nome) //Fabio
+
+// notação de colchetes
+console.log(pessoa["idade"]) //31
+
+// Desestruturar objetos
+const {nome,idade,cidade} = pessoa
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+
+const filmes = [
+    {
+        id:'1',
+        titulo: 'teste 1',
+        descricao: 'bla bla bla bla bla',
+        duracao: 120
+    },
+    {
+        id:'2',
+        titulo: 'teste 2',
+        descricao: 'ble ble ble ble ble',
+        duracao: 120
+    },
+    {
+        id:'3',
+        titulo: 'teste 3',
+        descricao: 'bli bli bli bli bli',
+        duracao: 120
+    }
+]
+
+const [{id,titulo,descricao,duracao}] = filmes
+
+filmes.map(filme => console.log(filme.descricao))

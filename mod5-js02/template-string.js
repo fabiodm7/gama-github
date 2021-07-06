a6b6c37206ada73cff39b28405dd44cdb1f9cddd
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-05 22:32:09 -0300

    operador ternario

--- mod5-js02/template-string.js
+++ mod5-js02/template-string.js
@@ -1,5 +1,24 @@
-// Interpolação
-"Fabio é"
-let role = "bom demais" 
+// // Interpolação
+// "Fabio é"
+// let role = "bom demais" 
 
-console.log(`Fabio é ${role}`)
+// console.log(`Fabio é ${role}`)
+
+// Operador ternário da
+
+// if...else...
+let id = 17
+function calculaIdade(idade){
+    if (idade >= 18){
+        return 'pode beber'
+    } else {
+        return 'não pode beber'
+    }
+}
+// ===
+function calculaIdade2(idade){
+    return idade >= 18 ? 'pode beber' : 'não pode beber'
+}
+
+console.log(calculaIdade(id))
+console.log(calculaIdade2(id))

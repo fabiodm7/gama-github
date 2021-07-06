a50b6f18f5a7f5f94f04b10406da7aa8ba9d9cd7
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-05 22:40:01 -0300

    operador ternario

--- mod5-js02/template-string.js
+++ mod5-js02/template-string.js
@@ -6,19 +6,36 @@
 
 // Operador ternário da
 
-// if...else...
-let id = 17
-function calculaIdade(idade){
-    if (idade >= 18){
-        return 'pode beber'
-    } else {
-        return 'não pode beber'
-    }
-}
-// ===
-function calculaIdade2(idade){
-    return idade >= 18 ? 'pode beber' : 'não pode beber'
+// // if...else...
+// let id = 17
+// function calculaIdade(idade){
+//     if (idade >= 18){
+//         return 'pode beber'
+//     } else {
+//         return 'não pode beber'
+//     }
+// }
+// // ===
+// function calculaIdade2(idade){
+//     return idade >= 18 ? 'pode beber' : 'não pode beber'
+// }
+
+// console.log(calculaIdade(id))
+// console.log(calculaIdade2(id))
+
+// Arrow functions
+// para funções anonimas ou funções de callback, funções utilizadas como parâmetros
+
+function soma(a,b){
+    return a + b
 }
 
-console.log(calculaIdade(id))
-console.log(calculaIdade2(id))
+console.log(soma(3,5)) //8
+
+// ===
+
+const soma2 = (num1,num2) => num1 + num2
+
+const sayHello = name => `Hello ${name}`
+
+console.log(sayHello('Fabio'))

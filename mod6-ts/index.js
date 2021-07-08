c3e850832257596891b6723d747e5c507c9df4c0
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-08 07:39:16 -0300

    iniciando ts

---
+++ mod6-ts/index.js
@@ -0,0 +1,8 @@
+function somar(num1,num2){
+    return num1 + num2;
+}
+
+const meuNum1 = '10';
+const meuNum2 = '5';
+
+console.log(somar(meuNum1,meuNum2));
---
+++ mod6-ts/index.ts
@@ -0,0 +1,8 @@
+function somar(num1:number,num2:number){
+    return num1 + num2;
+}
+
+const meuNum1 = 10;
+const meuNum2 = 5;
+
+console.log(somar(meuNum1,meuNum2));

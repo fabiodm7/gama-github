6b69b365ae1a51136279f648ddec2819c33c41a9
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-10 22:10:17 -0300

    importando as rotas no componente app

--- mod7-react/src/App.js
+++ mod7-react/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+
+import Routes from './routes';
 
 // não pode usar class por ser uma palavra reservada do JS
 // quando não queremos retornar tags dentro de uma tag específica utilizamos <></>
 // para importar um arquivo no diretório a sintaxe correta é ./nomeArquivo.extensao
 
 function App(props) {
-    const [ usuario, setUsuario ] = useState('');
-    function handlePesquisa(){
-        //console.log(usuario);
-        axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-    }
-    return (
-        <>
-            <h1>{props.title} {props.user}</h1>
-            <p>{ usuario }</p>
-            <input className="usuarioInput" placeholder="Usuario" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-            <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-        </>
+    return (  
+        <Routes />
     );
 }
 

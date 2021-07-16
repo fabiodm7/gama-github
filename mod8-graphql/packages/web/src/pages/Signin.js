40d781e8156179e636292c4ff82779333a2d014a
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-15 23:58:11 -0300

    requisicao assincrona

--- mod8-graphql/packages/web/src/pages/Signin.js
+++ mod8-graphql/packages/web/src/pages/Signin.js
@@ -21,18 +21,60 @@
 // </body>
 // </html>
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Signin(){
+    const [ email, setEmail] = useState('');
+    
+    const [ password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://localhost:8000/authenticate', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            })
+        })
+            .then((response) => response.json())
+            .then(()=>{
+                console.log('Sucesso')
+            });
+    };
+
+    const handleEmailChage = (event) => setEmail(event.target.value);
+
+    const handlePasswordChage = (event) => setPassword(event.target.value);
+
     return (
-        <form action="/authenticate" method="POST">
+        // <form action="/authenticate" method="POST">
+        <form onSubmit={handleSubmit}>
             <fieldset>
-                <label for="email">E-mail:</label>
-                <input id="email" type="email" inputmode="email" name="email" autocomplete="username" />
+                <label htmlFor="email">E-mail:</label>
+                <input
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChage}
+                    inputMode="email"
+                    // name="email"
+                    autoComplete="username"
+                />
             </fieldset>
             <fieldset>
-                <label for="password">Senha:</label>
-                <input id="password" type="password" name="password" autocomplete="current-password" /> 
+                <label htmlFor="password">Senha:</label>
+                <input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChage}
+                    // name="password"
+                    autoComplete="current-password"
+                /> 
             </fieldset>
             <button type="submit">Entrar</button>
         </form>

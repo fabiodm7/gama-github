7546adc9ad022e6a7bd94a06ac692ad273783d83
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-15 23:26:22 -0300

    mudando o server para express

--- mod8-graphql/packages/server/src/main.js
+++ mod8-graphql/packages/server/src/main.js
@@ -1,82 +1,100 @@
-import { createServer } from 'http';
+// import { createServer } from 'http';
 // import { readFile } from 'fs';
 // import { resolve } from 'path';
-import { parse } from 'querystring';
+// import { parse } from 'querystring';
 
-// criar o servidor http
-const server = createServer((request,response) => {
-    switch (request.url) {
-        // configura um rota no endereço status
-        case '/status': {
-            response.writeHead(200, {
-                'Content-Type': 'application/json',
-            });
-            response.write(
-                JSON.stringify({
-                    status: "OK",
-                })
-            );
-            response.end();
-            break;
-        }
-        // // rota para home page
-        // case '/home': {
-        //     const path = resolve(__dirname,'./pages/home.html');
-        //     readFile(path, (error,file) => {
-        //         if (error) {
-        //             response.writeHead(500, 'Não foi possível processar o arquivo html');
-        //             response.end();
-        //             return;
-        //         }
-        //         else {
-        //             response.writeHead(200);
-        //             response.write(file);
-        //             response.end();
-        //             return;
-        //         }
-        //     }); 
-        //     break;
-        // }
-        // // rota para pagina de login
-        // case '/signin':{
-        //     const path = resolve(__dirname,'./pages/signin.html');
-        //     readFile(path, (error,file) => {
-        //         if (error) {
-        //             response.writeHead(500, 'Não foi possível processar o arquivo html');
-        //             response.end();
-        //             return;
-        //         }
-        //         else {
-        //             response.writeHead(200);
-        //             response.write(file);
-        //             response.end();
-        //             return;
-        //         }
-        //     }); 
-        //     break;
-        // }
-        case '/authenticate': {
-            let data = '';
-            request.on('data', (chunck) => {
-                data += chunck;
-            });
-            request.on('end', () => {
-                const params = parse(data);
-                // response.writeHead(301,{
-                //     location: '/home',
-                // });
-                response.end();
-            });
-            break;
-        }
-        // configura mensagem de erro 404
-        default: {
-            response.writeHead(404, 'Service not found');
-            response.end();
-            
-        }
-    }
+import express from 'express';
+
+const server = express();
+
+server.get('/status', (_,response) =>{
+    response.send({
+        status: "OK",
+    });
 });
+
+server.post('/authenticate', express.json(), (request,response) =>{
+    console.log(
+        'Email', request.body.email,
+        'Senha', request.body.password,
+    );
+    response.send();
+});
+
+// // criar o servidor http
+// const server = createServer((request,response) => {
+//     switch (request.url) {
+//         // // configura um rota no endereço status
+//         // case '/status': {
+//         //     response.writeHead(200, {
+//         //         'Content-Type': 'application/json',
+//         //     });
+//         //     response.write(
+//         //         JSON.stringify({
+//         //             status: "OK",
+//         //         })
+//         //     );
+//         //     response.end();
+//         //     break;
+//         // }
+//         // // rota para home page
+//         // case '/home': {
+//         //     const path = resolve(__dirname,'./pages/home.html');
+//         //     readFile(path, (error,file) => {
+//         //         if (error) {
+//         //             response.writeHead(500, 'Não foi possível processar o arquivo html');
+//         //             response.end();
+//         //             return;
+//         //         }
+//         //         else {
+//         //             response.writeHead(200);
+//         //             response.write(file);
+//         //             response.end();
+//         //             return;
+//         //         }
+//         //     }); 
+//         //     break;
+//         // }
+//         // // rota para pagina de login
+//         // case '/signin':{
+//         //     const path = resolve(__dirname,'./pages/signin.html');
+//         //     readFile(path, (error,file) => {
+//         //         if (error) {
+//         //             response.writeHead(500, 'Não foi possível processar o arquivo html');
+//         //             response.end();
+//         //             return;
+//         //         }
+//         //         else {
+//         //             response.writeHead(200);
+//         //             response.write(file);
+//         //             response.end();
+//         //             return;
+//         //         }
+//         //     }); 
+//         //     break;
+//         // }
+//         case '/authenticate': {
+//             let data = '';
+//             request.on('data', (chunck) => {
+//                 data += chunck;
+//             });
+//             request.on('end', () => {
+//                 const params = parse(data);
+//                 // response.writeHead(301,{
+//                 //     location: '/home',
+//                 // });
+//                 response.end();
+//             });
+//             break;
+//         }
+//         // configura mensagem de erro 404
+//         default: {
+//             response.writeHead(404, 'Service not found');
+//             response.end();
+            
+//         }
+//     }
+// });
 
 // define uma variável para a porta do servidor, deve ser preenchida no start do servidor 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;

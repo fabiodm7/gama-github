0688ef449ebb707998125f75dbb7c26743df9981
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-16 08:46:44 -0300

    Criando GraphQL Queries erro

--- mod8-graphql/packages/server/src/graphql/resolvers.js
+++ mod8-graphql/packages/server/src/graphql/resolvers.js
@@ -1,10 +1,13 @@
 import { resolvers as demandResolvers } from './Demand/Demand'
+import { resolvers as clientResolvers } from './Client/Client'
 
 const resolvers = {
     ...demandResolvers,
+    ...clientResolvers,
 
     Query: {
         ...demandResolvers.Query,
+        ...clientResolvers.Query,
     },
 };
 

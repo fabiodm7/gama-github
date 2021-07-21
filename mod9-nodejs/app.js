40a26f3eb6532f034233339744aacf3ca38b1a75
fabiodm7 <fabio.d.m@hotmail.com>
2021-07-21 00:24:16 -0300

    Pratica de desenvolvimento em javascript

--- mod9-nodejs/app.js
+++ mod9-nodejs/app.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const url = require('url');
 const queryString = require('query-string');
+const fs = require('fs'); 
 
 // definindo o endereço/url
 const hostname = '127.0.0.1'; // localhost
@@ -10,23 +11,77 @@ const port = 3000;
 // implementação da regra de negócio
 const server = http.createServer((req,res) => {
 
-    // pegar a pergunta na url
-    const params = queryString.parse(url.parse(req.url).search);
-    
-    // verificar a pergunta e escolher uma resposta
     let resposta;
-    if (params.pergunta == 'autor'){
-        resposta = 'Fabio Duarte Medina';
-    } else if (params.pergunta == 'cidade'){
-        resposta = 'Sao Paulo';
-    } else{
-        resposta = 'Sei la meu';
+    const urlparse = url.parse(req.url);
+    // receber informacoes do usuario
+    const params = queryString.parse(urlparse.search);
+
+    function criaUsuario(params){
+
+        // criar usuario
+        // salvar informacoes
+        fs.writeFile('users/' + params.id + '.json', JSON.stringify(params), function (err) {
+            if (err) throw err;
+            console.log('Salvo!');
+            resposta = 'Usuario criado com sucesso'
+            
+            // retornar a resposta escolhida
+            res.statusCode = 200;
+            res.setHeader('Content-Type','text/plain');
+            res.end(resposta);
+        }); 
+    };
+    function buscaUsuario(params){
+        
+    };
+    function imprimeUsuario(params){
+        
+        // selecionar usuario
+        fs.readFile('users/' + params.id + '.json', function(err, data) {
+            if (err) {
+                resposta = 'Usuario nao existe';
+                
+                // retornar a resposta escolhida
+                res.statusCode = 200;
+                res.setHeader('Content-Type','text/plain');
+                res.end(resposta);
+            } else {
+    
+                resposta = data;
+                console.log('Founded! ' + data)
+        
+                // retornar a resposta escolhida
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.end(resposta);
+            }
+        });
+    }
+    function apagaUsuario(params){
+
+        // apaga usuario
+        fs.unlink('users/' + params.id + '.json', function (err) {
+            // if (err) throw err;
+            // console.log('File deleted!');
+            resposta = err ? 'Usuario nao existe' : 'Usuario apagado';
+    
+            // retornar a resposta escolhida
+            res.statusCode = 200;
+            res.setHeader('Content-Type','text/plain');
+            res.end(resposta);
+        });
+    };
+
+    if (urlparse.pathname == '/criausuario') {
+        criaUsuario(params);
+    } else if (urlparse.pathname == '/atualizausuario') {
+        criaUsuario(params);
+    } else if (urlparse.pathname == '/buscausuario') {
+        imprimeUsuario(params);     
+    } else if (urlparse.pathname == '/apagausuario') {
+        apagaUsuario(params);
     }
 
-    // retornar a resposta escolhida
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end(resposta);
 });
 
 // execução
